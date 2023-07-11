8f90bb66a64d3f8c11ac4b388d430b1991e799aa
Mohamad Daher <madaher@gmail.com>
2023-07-11 16:26:23 +0300

    added config to middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export { withAuth } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 // export const config = {
 //   // matcher: ["/profile"],
 //   matcher: ["/((?!api|login).*)"],
@@ -7,5 +10,14 @@ export { default } from "next-auth/middleware";
 // export const config = {
 //   matcher: ["/((?!api|login|^$).*)"],
 // };
-
+export default withAuth(
+  function middleware(req) {
+    console.log("middleware");
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.role === "admin",
+    },
+  }
+);
 export const config = { matcher: ["/(dashboard|profile)"] };
